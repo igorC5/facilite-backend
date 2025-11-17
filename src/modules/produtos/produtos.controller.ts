bfbf69acb82464bde4dbabb72548bcc6131eafe3
igorC5 <igorcandidofs@gmail.com>
2025-11-17 21:21:53 -0200

    crud de produtos

--- src/modules/produtos/produtos.controller.ts
+++ src/modules/produtos/produtos.controller.ts
@@ -1,21 +1,40 @@
-// src/produto/produto.controller.ts
-import { Body, Controller, Post, Get } from '@nestjs/common';
+import { Body, Controller, Post, Get, Delete, Param, Put } from '@nestjs/common';
 import { ProdutoService } from './produtos.service';
 
 @Controller('produtos')
 export class ProdutoController {
   constructor(private readonly produtoService: ProdutoService) {}
 
-  @Post()
-  async criarProduto(
-    @Body() body: { nome: string; preco_venda: number },
-  ) {
-    console.log(body);
-    return this.produtoService.criarProduto(body);
-  }
-
   @Get()
   async listarProdutos() {
     return this.produtoService.listarProdutos();
   }
+
+  @Get(':id')
+  async buscarProduto(@Param('id') id: string) {
+    return this.produtoService.buscarProduto(Number(id));
+  }
+
+  @Post()
+  async criarProduto(
+    @Body() body: { nome: string; preco_venda: number },
+  ) {
+    return this.produtoService.criarProduto(body);
+  }
+
+  @Put(':id')
+  async atualizarProduto(
+    @Param('id') id: number,
+    @Body() body: { nome: string; preco_venda: number }
+  ) {
+    console.log(id);
+    console.log(body);
+    return this.produtoService.atualizarProduto(Number(id), body);
+  }
+
+  @Delete(':id')
+  async excluirProduto(@Param('id') id: string) {
+    return this.produtoService.excluirProduto(Number(id));
+  }
+
 }
--- src/modules/produtos/produtos.service.ts
+++ src/modules/produtos/produtos.service.ts
@@ -1,10 +1,19 @@
-// src/produto/produto.service.ts
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/infra/prisma/prisma.service';
 
 @Injectable()
 export class ProdutoService {
   constructor(private prisma: PrismaService) {}
+  
+  async listarProdutos() {
+    return this.prisma.produto.findMany();
+  }
+
+  async buscarProduto(id: number) {
+    return this.prisma.produto.findUnique({
+      where: { id },
+    });
+  }
 
   async criarProduto(data: { nome: string; preco_venda: number }){
     return this.prisma.produto.create({
@@ -12,7 +21,18 @@ export class ProdutoService {
     });
   }
 
-  async listarProdutos() {
-    return this.prisma.produto.findMany();
+  async atualizarProduto(id: number, data: { nome: string; preco_venda: number }) {
+    return this.prisma.produto.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async excluirProduto(produtoId: number) {
+    return this.prisma.produto.delete({
+      where: {
+        id: produtoId,
+      }
+    });
   }
 }
